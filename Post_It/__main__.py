20faa4cc28c38b1413217093e0a16a46fb6eb03b
MintzyG <ericbraga2000@gmail.com>
2024-02-09 23:52:21 -0300

    Created a nix flake for easily developing on linux

--- Post_It/__main__.py
+++ Post_It/__main__.py
@@ -163,4 +163,4 @@ if os.path.isfile('./Post_It/temp/images.json'):
 init()
 create_app() 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="443", debug=True)
+    app.run(host="127.0.0.1", port="5000", debug=True)
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  outputs = {nixpkgs, ... }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+	config.allowUnfree = true;
+      };
+    in 
+      pkgs.mkShell {
+        name = "Post-It";
+        packages = with pkgs; [
+          python310
+	  python310Packages.tweepy
+	  python310Packages.flask
+	  pkgs.python310Packages.tkinter
+	  python310Packages.flask-restful
+	  python310Packages.requests-oauthlib
+        ];
+      };
+  };
+}
